7357ba8a5fc58cc48e4ad8967d3b6fc1b7cfe230
huantingwei <ting02940@gmail.com>
2020-06-27 22:34:25 +0800

    debug: turn on debugging mode temporarily for debugging

--- ross/production_settings.py
+++ ross/production_settings.py
@@ -7,4 +7,4 @@ DATABASES = {
 STATIC_ROOT = 'staticfiles' # 設定網站正式上線時靜態檔案目錄位置
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # 設定HTTP連線方式
 ALLOWED_HOSTS = ['*'] # 讓所有的網域都能瀏覽本網站
-DEBUG = False # 關閉除錯模式
+DEBUG = True # 關閉除錯模式
